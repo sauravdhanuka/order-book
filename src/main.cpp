6e9a960f1449002b11d34a68fd41de051d353eac
sauravdhanuka <sauravdhanuka7@gmail.com>
2026-02-21 13:19:00 +0530

    update files

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,29 @@
 #include "csv_parser.h"
 #include <iostream>
 #include <fstream>
+#include <memory>
 
 int main(int argc, char* argv[]) {
-    ob::MatchingEngine engine;
-    ob::CsvParser parser(engine);
+    try {
+        auto engine = std::make_unique<ob::MatchingEngine>();
+        ob::CsvParser parser(*engine);
 
-    if (argc > 1) {
-        // Read from file
-        std::ifstream file(argv[1]);
-        if (!file.is_open()) {
-            std::cerr << "Error: cannot open file '" << argv[1] << "'\n";
-            return 1;
+        if (argc > 1) {
+            std::ifstream file(argv[1]);
+            if (!file.is_open()) {
+                std::cerr << "Error: cannot open file '" << argv[1] << "'\n";
+                return 1;
+            }
+            parser.process_stream(file, std::cout);
+        } else {
+            parser.process_stream(std::cin, std::cout);
         }
-        parser.process_stream(file, std::cout);
-    } else {
-        // Read from stdin
-        parser.process_stream(std::cin, std::cout);
+    } catch (const std::exception& e) {
+        std::cerr << "UNHANDLED EXCEPTION: " << e.what() << '\n';
+        return 1;
+    } catch (...) {
+        std::cerr << "UNHANDLED UNKNOWN EXCEPTION\n";
+        return 1;
     }
 
     return 0;
